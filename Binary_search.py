1080da2128d5ea225dcd8686d37ab3337ba06616
GodsonTech <73137917+GodsonTech@users.noreply.github.com>
2023-04-05 11:05:16 +0100

    Create Binary_search.py

---
+++ Binary_search.py
@@ -0,0 +1,15 @@
+def binary_search(list, item):
+  low = 0
+  high = len(list)-1 
+  
+  while low <= high:
+     mid = (low + high)
+     guess = list[mid] #square bracket to access the middle list element 
+    
+    if guess == item:
+      return mid
+    if guess < item:
+      low = mid + 1
+     else:
+      high = mid - 1
+   return None 
